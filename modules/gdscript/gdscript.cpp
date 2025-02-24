c9eb53860c3aac76f7369d9d3c1f084d7b0982e8
Robert Yevdokimov <105675984+ryevdokimov@users.noreply.github.com>
2025-02-25 00:31:06 +0400

    Allow tool scripts to run processes as soon as they are attached

--- modules/gdscript/gdscript.cpp
+++ modules/gdscript/gdscript.cpp
@@ -898,6 +898,7 @@ Error GDScript::reload(bool p_keep_state) {
 #endif
 
 	reloading = false;
+	emit_signal("reloaded");
 	return OK;
 }
 
@@ -1076,6 +1077,7 @@ void GDScript::_get_property_list(List<PropertyInfo> *p_properties) const {
 
 void GDScript::_bind_methods() {
 	ClassDB::bind_vararg_method(METHOD_FLAGS_DEFAULT, "new", &GDScript::_new, MethodInfo("new"));
+	ADD_SIGNAL(MethodInfo("reloaded"));
 }
 
 void GDScript::set_path_cache(const String &p_path) {
